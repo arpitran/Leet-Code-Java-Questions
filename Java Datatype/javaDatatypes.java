79c700f23ecf538656044f367472b2cdc60545da
arpitran <35965795+arpitran@users.noreply.github.com>
2019-02-24 21:15:31 -0500

    Update javaDatatypes.java

--- Java Datatype/javaDatatypes.java
+++ Java Datatype/javaDatatypes.java
@@ -21,8 +21,8 @@ class Solution{
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)System.out.println("* byte");
                 if(x>=-32768 && x<=32767)System.out.println("* short");
-                if(x>=-Math.pow(2,31) && x<=Math.pow(2,31))System.out.println("* int");
-                if(x>=-Math.pow(2,63) && x<=Math.pow(2,63))System.out.println("* long");
+                if(x>=-2147483648 && x<=2147483647)System.out.println("* int");
+                System.out.println("* long"); 
                 
             }
             catch(Exception e)
